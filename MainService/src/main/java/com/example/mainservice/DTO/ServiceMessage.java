2df84202473d054e115083a977d588f47eaa695e
codd <102369774+jj0rd@users.noreply.github.com>
2026-01-26 19:01:51 +0100

    History back+front

--- MainService/src/main/java/com/example/mainservice/DTO/ServiceMessage.java
+++ MainService/src/main/java/com/example/mainservice/DTO/ServiceMessage.java
@@ -5,10 +5,39 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ServiceMessage {
     private String serviceName;
     private String content;
     private double weight;
+
+    public ServiceMessage(String serviceName, String content, double weight) {
+        this.serviceName = serviceName;
+        this.content = content;
+        this.weight = weight;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 }
