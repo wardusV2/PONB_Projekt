683ae81bc48aae5cd78f4389b791f941a19e2211
wardusV2 <p.anonim88@gmail.com>
2026-01-27 11:20:24 +0100

    fix

--- SafeLogin-master/src/main/java/com/webproject/safelogin/controller/RecommendationController.java
+++ SafeLogin-master/src/main/java/com/webproject/safelogin/controller/RecommendationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/recommendations")
+@RequestMapping("/recommendations")
 public class RecommendationController {
 
     private final RecommendationService recommendationService;
