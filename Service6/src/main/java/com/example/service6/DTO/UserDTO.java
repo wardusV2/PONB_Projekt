76a316864954cbbc1bfd7e47c87ca8abaa144c2f
wardusV2 <p.anonim88@gmail.com>
2026-01-29 12:55:20 +0100

    fix

---
+++ Service6/src/main/java/com/example/service6/DTO/UserDTO.java
@@ -0,0 +1,7 @@
+package com.example.service6.DTO;
+
+public record UserDTO(
+        int id,
+        String email,
+        String nick) {
+}
---
+++ Service7/src/main/java/com/example/service7/DTO/UserDTO.java
@@ -0,0 +1,7 @@
+package com.example.service7.DTO;
+
+public record UserDTO(
+        int id,
+        String email,
+        String nick) {
+}
